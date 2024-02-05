fb3b56bd7adfa61b50061df0d50cb0672952821a
Mathias home <au568660@uni.au.dk>
2024-02-05 19:54:16 +0100

    installed gorilla mux

--- main.go
+++ main.go
@@ -1,58 +1,74 @@
 package main
 
-import (
+import ( // crypto import
 	"crypto/aes"
-	"fmt"
 	"encoding/hex"
+
+	"fmt"//general import
+
+    
+	"log"
+	"net/http"
+	"github.com/gorilla/mux" 
+	//net import
 )
 
 func main() {
- 
-    // cipher key
-    key := "thisis32bitlongpassphraseimusing"
- 
-    // plaintext
-    pt := "This is a secret"
- 
-    c := EncryptAES([]byte(key), pt)
- 
-    // plaintext
-    fmt.Println(pt)
- 
-    // ciphertext
-    fmt.Println(c)
- 
-    // decrypt
-    DecryptAES([]byte(key), c)
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, world!")
+	})
+	log.Print("Server starting at localhost:4444")
+	http.ListenAndServe(":4444", r)
 }
- 
+
+func cipher() {
+
+	// cipher key
+	key := "thisis32bitlongpassphraseimusing"
+
+	// plaintext
+	pt := "This is a secret"
+
+	c := EncryptAES([]byte(key), pt)
+
+	// plaintext
+	fmt.Println(pt)
+
+	// ciphertext
+	fmt.Println(c)
+
+	// decrypt
+	DecryptAES([]byte(key), c)
+}
+
 func EncryptAES(key []byte, plaintext string) string {
- 
-    c, err := aes.NewCipher(key)
-    CheckError(err)
- 
-    out := make([]byte, len(plaintext))
- 
-    c.Encrypt(out, []byte(plaintext))
- 
-    return hex.EncodeToString(out)
+
+	c, err := aes.NewCipher(key)
+	CheckError(err)
+
+	out := make([]byte, len(plaintext))
+
+	c.Encrypt(out, []byte(plaintext))
+
+	return hex.EncodeToString(out)
 }
- 
+
 func DecryptAES(key []byte, ct string) {
-    ciphertext, _ := hex.DecodeString(ct)
- 
-    c, err := aes.NewCipher(key)
-    CheckError(err)
- 
-    pt := make([]byte, len(ciphertext))
-    c.Decrypt(pt, ciphertext)
- 
-    s := string(pt[:])
-    fmt.Println("DECRYPTED:", s)
+	ciphertext, _ := hex.DecodeString(ct)
+
+	c, err := aes.NewCipher(key)
+	CheckError(err)
+
+	pt := make([]byte, len(ciphertext))
+	c.Decrypt(pt, ciphertext)
+
+	s := string(pt[:])
+	fmt.Println("DECRYPTED:", s)
 }
- 
+
 func CheckError(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
